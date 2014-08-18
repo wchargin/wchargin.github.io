3d27069942042d43f783452be15d3c35606fa0c9
William Chargin <wchargin@gmail.com>
2014-08-17 19:38:21 -0700

    Write content for home page

---
+++ js/email.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.email-link').click(function() {
+        window.open('http://www.google.com/recaptcha/mailhide/d?k\07501j2xJSp6GMIhivlCpmSrxBw\75\75\46c\75F0-eweu468thgo0QAg6G9LcFdYCxAAOHiwDXZtIR9Gk\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300');
+        return false;
+    });
+});
