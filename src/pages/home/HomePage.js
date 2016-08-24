33791940d2d698c24dc7b882365bc5a48d805a86
William Chargin <wchargin@gmail.com>
2016-08-24 10:51:53 -0700

    Use Blurb on the home page
    Summary:
    This way, the image positioning is consistent across pages.

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,9 +3,8 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, css} from 'aphrodite';
 
-import {Heading, Link, MailLink} from '../../Components';
+import {Blurb, Heading, Link, MailLink} from '../../Components';
 
 Link.registerPreloadResources('/', () => [
     require('./me.png'),
@@ -22,48 +21,42 @@ export default class HomePage extends Component {
 
         return <div>
             <Heading level={1}>Hey!</Heading>
-            <img
-                src={require("./me.png")}
-                alt="me"
-                className={css(styles.face)}
-            />
-            <p>
-                I&rsquo;m pursuing a computer science major and math minor
-                at <Link href={urls.cmu}>Carnegie Mellon</Link>.
-                I&nbsp;love computer science because it unites math, logic,
-                and critical thinking into a powerful toolset that I&nbsp;can
-                use to solve real-world problems. You&rsquo;ve landed on my
-                personal website and portfolio.
-            </p>
-            <p>
-                Helpful info and links:
-            </p>
-            <ul>
-                <li>
-                    Pick up a copy of
-                    my <Link href={urls.resume}>r&eacute;sum&eacute;!</Link>
-                </li>
-                <li>
-                    Feel free to check out{" "}
-                    <Link href={urls.github}>my GitHub profile!</Link>
-                </li>
-                <li>
-                    If you&rsquo;d like to contact me,{" "}
-                    <MailLink>request my email!</MailLink>
-                </li>
-            </ul>
-            <p>
-                My surname is pronounced with a hard&nbsp;<i>g</i>: it rhymes
-                with&nbsp;<i>bargain</i>, not&nbsp;<i>barge&nbsp;in</i>.
-            </p>
+            <Blurb
+                image={require("./me.png")}
+                imagePosition={Blurb.IMAGE_RIGHT}
+            >
+                <p>
+                    I&rsquo;m pursuing a computer science major and math minor
+                    at <Link href={urls.cmu}>Carnegie Mellon</Link>.
+                    I&nbsp;love computer science because it unites math, logic,
+                    and critical thinking into a powerful toolset that
+                    I&nbsp;can use to solve real-world problems. You&rsquo;ve
+                    landed on my personal website and portfolio.
+                </p>
+                <p>
+                    Helpful info and links:
+                </p>
+                <ul>
+                    <li>
+                        Pick up a copy of my{" "}
+                        <Link href={urls.resume}>r&eacute;sum&eacute;!</Link>
+                    </li>
+                    <li>
+                        Feel free to check out{" "}
+                        <Link href={urls.github}>my GitHub profile!</Link>
+                    </li>
+                    <li>
+                        If you&rsquo;d like to contact me,{" "}
+                        <MailLink>request my email!</MailLink>
+                    </li>
+                </ul>
+                <p>
+                    My surname is pronounced with a hard&nbsp;<i>g</i>: it
+                    rhymes with&nbsp;<i>bargain</i>,
+                    not&nbsp;<i>barge&nbsp;in</i>.
+                </p>
+            </Blurb>
         </div>;
     }
 
 }
-
-const styles = StyleSheet.create({
-    face: {
-        float: 'right',
-        borderRadius: '100%',
-    },
-});
