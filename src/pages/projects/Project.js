063f074615bc1df8db1af05a6833e7f7ec7b166f
William Chargin <wchargin@gmail.com>
2016-08-24 22:27:02 -0700

    Add a component for individual project pages

---
+++ src/pages/projects/Project.js
@@ -0,0 +1,40 @@
+/*
+ * A generic component for an individual project page.
+ */
+
+import React, {Component, PropTypes} from 'react';
+
+import {Heading, Link} from '../../Components';
+
+export default class Project extends Component {
+
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        heroImage: PropTypes.string,  // url
+        children: PropTypes.node.isRequired,
+    }
+
+    render() {
+        const renderBackLink = () => <Link
+            to="/projects"
+            style={{marginBottom: 10, marginTop: 10}}
+        >
+            <i>&laquo;&nbsp;back to projects</i>
+        </Link>;
+        return <div>
+            <Heading level={1}>{this.props.name}</Heading>
+            {renderBackLink()}
+            {this.props.heroImage &&
+                <div style={{
+                    background: 'center no-repeat',
+                    backgroundImage:
+                        `url(${JSON.stringify(this.props.heroImage)})`,
+                    marginBottom: 10,
+                    height: 200,
+                }} />}
+            {this.props.children}
+            {renderBackLink()}
+        </div>;
+    }
+
+}
