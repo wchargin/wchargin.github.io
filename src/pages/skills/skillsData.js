42791719e296ab22f93bd8f603d1bce43e3aedc5
William Chargin <wchargin@gmail.com>
2016-08-28 10:04:33 -0400

    Add JavaScript/React skill contents

--- src/pages/skills/skillsData.js
+++ src/pages/skills/skillsData.js
@@ -39,9 +39,7 @@ export const createSkillsData = () => [
         {
             name: 'JavaScript and React',
             image: require('./js-react.png'),
-            description: <p>
-                Nothing goes over my head; my reflexes are too fast.
-            </p>,
+            description: renderJSReactDescription(),
         },
         {
             name: 'Java',
@@ -126,5 +124,71 @@ function renderPythonDescription() {
                 &ldquo;<tt>hasattr</tt>: a dangerous misnomer.&rdquo;
             </Link>
         </p>
+    </div>;
+}
+
+function renderJSReactDescription() {
+    const urls = {
+        lc3App: "https://wchargin.github.io/lc3web/",
+        lc3Source: "https://wchargin.github.io/lc3web/",
+        lc3ReactSource: "https://wchargin.github.io/lc3/",
+        vareseApp: "https://wchargin.github.io/varese/",
+        vareseSource: "https://github.com/wchargin/varese",
+        vareseTreeView: "https://wchargin.github.io/varese/#/infinite-tree",
+        vareseTreeSource: "https://github.com/wchargin/varese/blob/master/src/core/CanvasCore.js",
+        vareseTreeTest: "https://github.com/wchargin/varese/blob/master/test/core/CanvasCoreTest.js",
+        siteSource: "https://github.com/wchargin/wchargin.github.io",
+    };
+    return <div>
+        <p>
+            Some relevant projects include:
+        </p>
+        <ul>
+            <li><p>
+                <Link href={urls.lc3App}>
+                    An assembler and interpreter for the&nbsp;LC-3, a
+                    teaching microcomputer
+                </Link>{" "}
+                (<Link href={urls.lc3Source}>source on GitHub</Link>).
+                This is a single-page jQuery application currently that
+                I&nbsp;wrote to replace the course-provided interpreter for my
+                computer architecture course at Cal&nbsp;Poly; it continues to
+                be used by students of that course. (I&nbsp;also started{" "}
+                <Link href={urls.lc3ReactSource}>porting it to React</Link>,
+                and rewrote the assembler with full test coverage, but
+                abandoned that port a while ago.)
+            </p></li>
+            <li><p>
+                <Link href={urls.vareseApp}>
+                    A set of tools for a music professor of mine
+                </Link>{" "}
+                (<Link href={urls.vareseSource}>
+                    source and explanation on GitHub
+                </Link>).
+                This is a React application that, I&rsquo;m&nbsp;pleased to
+                report, has 100%&nbsp;test coverage, including the
+                GUI&nbsp;components. I&rsquo;m&nbsp;particularly proud of the
+                implementation for{" "}
+                <Link href={urls.vareseTreeView}>
+                    the infinite tree view
+                </Link>, an infinitely scrolling canvas from dynamically
+                generated data; all the logic for the scrolling, positioning,
+                and rendering is{" "}
+                <Link href={urls.vareseTreeSource}> purely functional</Link>
+                {" "}and{" "}
+                <Link href={urls.vareseTreeTest}>extensively tested!</Link>
+            </p></li>
+            <li><p>
+                This very site, also written in React!
+                (<Link href={urls.siteSource}>
+                    The source is on GitHub.
+                </Link>)
+                Everything is server-side rendered to a static site, so
+                there&rsquo;s no lag before the initial render and the site
+                should function just fine even with JavaScript disabled. After
+                rehydration, client-side routing takes over to enable instant
+                page loads, as well as interactive components like this one.
+            </p></li>
+        </ul>
     </div>;
 }
